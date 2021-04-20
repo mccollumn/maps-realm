4f15dd14934c7e7199b1b005e8f2f5f5e1153215
Nick McCollum <nmccollum@frontier.com>
2021-04-19 18:04:31 -0700

    Realm getLocationById function

---
+++ functions/getLocationById.js
@@ -0,0 +1,11 @@
+exports = async function getLocationById(id) {
+  console.log("Location ID:", id);
+  const mongodb = context.services.get("mongodb-atlas");
+  const locations = mongodb.db("maps").collection("locations");
+  const results = await locations.findOne({ _id: BSON.ObjectId(id) });
+  const stringified = JSON.stringify(results);
+
+  return {
+    data: JSON.parse(stringified),
+  };
+};
