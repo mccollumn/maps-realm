334721995e2ec20ae906c310d82d1abfb1f105ef
Nick McCollum <nmccollum@frontier.com>
2021-05-19 18:03:41 -0700

    getAllLocations function

--- functions/getAllLocations.js
+++ functions/getAllLocations.js
@@ -1,7 +1,8 @@
-exports = async function getLocationById(id) {
+exports = async function getAllLocations() {
   const mongodb = context.services.get("mongodb-atlas");
   const locations = mongodb.db("maps").collection("locations");
-  const results = await locations.findOne({ _id: BSON.ObjectId(id) });
+  const cursor = await locations.find({});
+  const results = await cursor.toArray();
   const stringified = JSON.stringify(results);
 
   return JSON.parse(stringified);
